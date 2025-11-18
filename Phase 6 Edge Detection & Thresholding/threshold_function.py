ed721582e8666833515c3983111aa7d3bbed614f
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-11-18 19:16:44 +0530

    Add image thresholding function and output file
    Introduced an apply_threshold function in threshold_function.py to perform binary thresholding on grayscale images using OpenCV. Added example usage to read, process, save, and display the thresholded image. Also added the resulting thresholded_image.jpg to the repository.

--- Phase 6 Edge Detection & Thresholding/threshold_function.py
+++ Phase 6 Edge Detection & Thresholding/threshold_function.py
@@ -1,1 +1,25 @@
 import cv2
+
+# Thresholding function
+def apply_threshold(image_path, threshold_value):
+    # Load the image
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    
+    # Apply binary thresholding
+    _, thresholded_image = cv2.threshold(image, threshold_value, 255, cv2.THRESH_BINARY)
+    
+    return thresholded_image
+# Example usage
+if __name__ == "__main__":
+    image_path = 'Phase 6 Edge Detection & Thresholding\\img.jpg'
+    threshold_value = 128
+    
+    thresholded_image = apply_threshold(image_path, threshold_value)
+    
+    # Save the thresholded image
+    cv2.imwrite('thresholded_image.jpg', thresholded_image)
+    
+    # Display the thresholded image
+    cv2.imshow('Thresholded Image', thresholded_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
